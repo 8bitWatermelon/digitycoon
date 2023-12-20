e66c3499c97dff68b0691640f6a374852cdfdde4
_Enoabasi_Essien_ <enoabasidream@gmail.com>
2023-12-20 18:47:06 +0400

    build: component and site structuring
    - Project structure and setup

---
+++ constants/meta.ts
@@ -0,0 +1,9 @@
+export const meta = {
+  title: 'Digitycoon',
+  description: 'Your go to destination for digital products.',
+
+  developer: {
+    name: '8Bitwatermelon',
+    year: '2023'
+  }
+}
